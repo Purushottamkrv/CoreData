a7067911ab66be73e607bc1a133003c59a4c6ef6
purushottamkrv <purushottam.verma@oodlestechnologies.com>
2019-09-07 01:25:28 +0530

    faff

--- CoreDataTest/CoreaDataHelper.swift
+++ CoreDataTest/CoreaDataHelper.swift
@@ -7,3 +7,32 @@
 //
 
 import Foundation
+import CoreData
+import UIKit
+import SwiftyJSON
+class CoreDataHelper{
+    
+    static let shaed = CoreDataHelper()
+    
+    func createData(response:JSON) {
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else { return}
+        let managedContext = appDelegate.persistentContainer.viewContext
+        let userEntity = NSEntityDescription.entity(forEntityName: "Categories", in: managedContext)!
+        let arrayValue = response["categories"].arrayValue
+        
+        for i in arrayValue{
+            let user = NSManagedObject(entity: userEntity, insertInto: managedContext)
+            user.setValue(i["category_slug"].stringValue, forKey: "category_slug")
+            user.setValue(i["bcategory_name"].stringValue, forKey: "bcategory_name")
+            user.setValue(i["icon"].stringValue, forKey: "icon")
+
+            }
+        
+        do {
+            try managedContext.save()
+        }catch{
+            print("failed saving")
+        }
+        
+    }
+}
--- CoreDataTest/ViewController.swift
+++ CoreDataTest/ViewController.swift
@@ -7,14 +7,52 @@
 //
 
 import UIKit
+import Alamofire
+import SwiftyJSON
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        Call()
         // Do any additional setup after loading the view.
     }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell") as! TableViewCell
+        cell.textLabel?.text = "prince"
+        return cell
+    }
+
+    
+    func Call(){
+        Alamofire.request("https://paygyft.com/api/v1/user/store/search/popular", method: .post, encoding: JSONEncoding.prettyPrinted, headers: Headers.header).responseJSON{ response in
+            print(response)
+            switch response.result{
+            case .success(let data):
+                let data = JSON(data)
+            
+            case .failure(let error):
+                print(error)
+            
+        }
+    }
+    
+}
+
 
 
 }
 
+
+
+
+struct Headers{
+    static let header:HTTPHeaders = [
+        "usrtoken" : "74c14fa62349c91c67607d8382656c431eb8e0b6-084e0343a0486ff05530df6c705c8bb4",
+        "lat" : "12.9716",
+        "lng" : "77.5946"]
+}
