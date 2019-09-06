13824f621ed4de2b8f41c9f068f69c2d408174a7
purushottamkrv <purushottam.verma@oodlestechnologies.com>
2019-09-07 01:19:03 +0530

    first commit

---
+++ CoreDataTest/CoreaDataHelper.swift
@@ -0,0 +1,9 @@
+//
+//  CoreaDataHelper.swift
+//  CoreDataTest
+//
+//  Created by Purushottam on 07/09/19.
+//  Copyright © 2019 Oodles. All rights reserved.
+//
+
+import Foundation
---
+++ CoreDataTest/Model.swift
@@ -0,0 +1,9 @@
+//
+//  Model.swift
+//  CoreDataTest
+//
+//  Created by Purushottam on 07/09/19.
+//  Copyright © 2019 Oodles. All rights reserved.
+//
+
+import Foundation
