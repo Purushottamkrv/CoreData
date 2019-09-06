8e70115c1e4caf256d8dc3f5e55cece1602a93ba
purushottamkrv <purushottam.verma@oodlestechnologies.com>
2019-09-07 00:08:22 +0530

    Initial Commit

---
+++ CoreDataTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoreDataTest
+//
+//  Created by Purushottam on 07/09/19.
+//  Copyright © 2019 Oodles. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
